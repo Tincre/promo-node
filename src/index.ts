bbbb96df7ecc2f35ca3e21959f3955069a864c81
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-12 17:37:47 -0400

    :bug: :wrench: add showWidget export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { generateAccessToken } from './promo-node-utils';
 import { getToken } from './getToken';
-import { getWidget, getWidgetCallback } from './cloudinary-utils';
+import { getWidget, getWidgetCallback, showWidget, } from './cloudinary-utils';
 import defaultOptions from './defaultOptions';
 import { VIDEO_EXTENSIONS, IMAGE_EXTENSIONS } from './constants';
 import { fetcher, detectMediaType, getOptions } from './utils';
@@ -9,6 +9,7 @@ export {
   getToken,
   getWidget,
   getWidgetCallback,
+  showWidget,
   defaultOptions,
   VIDEO_EXTENSIONS,
   IMAGE_EXTENSIONS,
