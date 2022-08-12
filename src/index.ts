fb1d624346755c7186487dd6788f510a444e27a5
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-12 17:43:29 -0400

    :recycle: :art: lint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { generateAccessToken } from './promo-node-utils';
 import { getToken } from './getToken';
-import { getWidget, getWidgetCallback, showWidget, } from './cloudinary-utils';
+import { getWidget, getWidgetCallback, showWidget } from './cloudinary-utils';
 import defaultOptions from './defaultOptions';
 import { VIDEO_EXTENSIONS, IMAGE_EXTENSIONS } from './constants';
 import { fetcher, detectMediaType, getOptions } from './utils';
