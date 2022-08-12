5877ac7b2c8f20172e637c6a32a2a36cb35f31a9
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2022-08-12 16:52:49 -0400

    :sparkles: add IMAGE_ and VIDEO_ *EXTENSIONS exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { generateAccessToken } from './promo-node-utils';
 import { getToken } from './getToken';
 import { getWidget, getWidgetCallback } from './cloudinary-utils';
-import defaultOptions from './defaultOptions'; 
-
-export { generateAccessToken, getToken, getWidget, getWidgetCallback, defaultOptions, };
+import defaultOptions from './defaultOptions';
+import { VIDEO_EXTENSIONS, IMAGE_EXTENSIONS } from './constants';
+export {
+  generateAccessToken,
+  getToken,
+  getWidget,
+  getWidgetCallback,
+  defaultOptions,
+  VIDEO_EXTENSIONS,
+  IMAGE_EXTENSIONS,
+};
