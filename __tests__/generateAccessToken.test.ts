2bbb71ffc65d27a7e35c3ec571d36536e99a7dd7
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-08-13 11:52:04 -0500

    :sparkles: :white_check_mark: add test for new sub usage per #3

---
+++ __tests__/generateAccessToken.test.ts
@@ -0,0 +1,40 @@
+import { generateAccessToken } from '../src/promo-node-utils';
+jest.setTimeout(40000);
+
+describe('generateAccessToken no sub', () => {
+  const clientSecret = 'test client secret'; // never do this in your client side js but this is safe due to it being a testing application alone
+  const appId = 'test app id';
+  const clientId = 'test client id';
+  const hostName = 'http://localhost:3000';
+  let accessTokenSigned: string = generateAccessToken(
+    hostName,
+    clientId,
+    appId,
+    clientSecret
+  );
+
+  it('returns a string', async () => {
+    expect(typeof accessTokenSigned).toBe('string');
+  });
+});
+
+describe('generateAccessToken with sub and expiration', () => {
+  const clientSecret = 'test client secret'; // never do this in your client side js but this is safe due to it being a testing application alone
+  const appId = 'test app id';
+  const clientId = 'test client id';
+  const hostName = 'http://localhost:3000';
+  const sub = 'test@tincre.com';
+  const expiration = 123456
+  let accessTokenSigned: string = generateAccessToken(
+    hostName,
+    clientId,
+    appId,
+    clientSecret,
+    expiration,
+    sub
+  );
+
+  it('returns a string', async () => {
+    expect(typeof accessTokenSigned).toBe('string');
+  });
+});
