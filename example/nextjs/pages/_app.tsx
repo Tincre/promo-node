e4c6df5736db85ee5829544e99fc3be04ac093fa
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-09-20 18:42:15 -0600

    :fire: remove superfluous example directory

--- example/nextjs/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app';
-import '../../../dist/promo-button.esm.css'; // @tincre/promo-button/bundle.css
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
