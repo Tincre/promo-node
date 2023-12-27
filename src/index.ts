2d315897c6c8d1b94c1fa865912ded515df9f41f
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-12-27 11:07:55 -0600

    :wrench: add exports from storage module #5

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,19 @@ import { getToken } from './getToken';
 import { getWidget, getWidgetCallback, showWidget } from './cloudinary-utils';
 import defaultOptions from './defaultOptions';
 import { VIDEO_EXTENSIONS, IMAGE_EXTENSIONS } from './constants';
-import { fetcher, detectMediaType, getOptions } from './utils';
+import {
+  fetcher,
+  detectMediaType,
+  getOptions,
+  generateFilePath,
+} from './utils';
+import {
+  uploadMedia,
+  getS3Client,
+  getS3PutUrl,
+  getS3ReadUrl,
+  S3Credentials,
+} from './storage';
 export {
   generateAccessToken,
   getToken,
@@ -16,4 +28,9 @@ export {
   fetcher,
   detectMediaType,
   getOptions,
+  uploadMedia,
+  getS3Client,
+  getS3PutUrl,
+  getS3ReadUrl,
 };
+export type { S3Credentials };
